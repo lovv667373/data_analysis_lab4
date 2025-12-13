f16b6ccc329a364fa9073c57d5a510f8de7d69f6
Timur Pavlov <timurpavlov281@gmail.com>
2025-12-13 11:45:48 +0300

    Библиотеки
    Импортировали библиотеки в файл

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import warnings
+import os
+from datetime import datetime
